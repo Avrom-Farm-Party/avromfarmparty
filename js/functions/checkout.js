8898fad1a2f4a5e20206f3bb38746de68192282f
Spencer Tweedy <spencer@spencertweedy.com>
2019-04-26 06:18:17 -0500

    another dump

--- js/functions/checkout.js
+++ js/functions/checkout.js
@@ -3,24 +3,30 @@ exports.handler = function(event, context, callback) {
     // See your keys here: https://dashboard.stripe.com/account/apikeys
     var stripe = require("stripe")("sk_test_NxBFZhiuRth7jsK4JGmaDoDS00ebRADj39");
 
-    (async () => {
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items: [{
-          name: 'T-shirt',
-          description: 'Comfortable cotton t-shirt',
-          images: ['https://example.com/t-shirt.png'],
-          amount: 500,
-          currency: 'usd',
-          quantity: 1,
-        }],
-        success_url: 'https://example.com/success',
-        cancel_url: 'https://example.com/cancel',
-      });
-    })();
+    var obj = JSON.parse(event.body);
+    var ticketQuantity = obj.ticketQuantity;
+    var ticketPrice = obj.ticketPrice * 100;
 
-    callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
+    stripe.checkout.sessions.create({
+      success_url: "https://example.com/success",
+      cancel_url: "https://example.com/cancel",
+      payment_method_types: ["card"],
+      line_items: [{
+        name: "Avrom Farm Party 2019",
+        amount: ticketPrice,
+        images: ["https://avromfarmparty.netlify.com/img/logo.png"],
+        currency: "usd",
+        quantity: ticketQuantity
+      }]
+    }, function(err, session) {
+      console.log("Stripe session created");
+      console.log("Session identifier: " + session.id);
+      console.log("Ticket quantity: " + session.display_items[0].quantity);
+      console.log("Ticket price: " + session.display_items[0].amount);
+
+      callback(null, {
+        statusCode: 200,
+        body: session.id
+      });
     });
 }
