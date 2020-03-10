4adc1c0ca75525de8472708358eeaf16aea59f66
Spencer Tweedy <spencer@spencertweedy.com>
2020-03-10 01:41:33 -0500

    add client js

---
+++ client.js
@@ -0,0 +1,15 @@
+// Calculate ticket order total
+
+var orderTotal = document.getElementById('checkoutButton');
+var ticketQuantity = document.getElementById('ticketQuantity');
+var ticketPrice = document.getElementById('ticketPrice');
+
+function calculateTotal() {
+  // Only update orderTotal if the ticket price is not NotaNumber
+  if (!isNaN(parseInt(ticketPrice.value))) {
+    orderTotal.value = "Pay $" + parseInt(ticketQuantity.value) * parseInt(ticketPrice.value);
+  }
+}
+
+// Bind calculateTotal to form's change event
+document.getElementById('ticket-form').addEventListener('change', calculateTotal);
