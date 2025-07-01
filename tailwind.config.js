e21b81af6b821a981694f77e22a73d0664277662
Spencer Tweedy <spencer@spencertweedy.com>
2025-07-01 18:45:06 -0400

    port into jekyll, and minor style changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.html"],
+  content: ["pages/**/*.html", "_layouts/**/*.html"],
   theme: {
     letterSpacing: {
       'supertight': '-0.07em'
